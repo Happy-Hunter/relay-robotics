6cfd0a8b03337cd3734fcec5da43466c577bf482
Loden <loden.gendun@students.mq.edu.au>
2022-05-20 14:45:29 +1000

    refactor

--- script/script.js
+++ script/script.js
@@ -4,32 +4,26 @@ $(() => {
     
     function handlerIn() {
         $mascot.attr('src', './css/images/mascot-closed-eye.png');
-    }
+    };
 
     function handlerOut() {
         $mascot.attr('src', './css/images/mascot.png');
-    }
+    };
 
     function submitHandler() {
         $mascot.attr('src', './css/images/mascot-happy.png');
         window.scrollTo(0, 0);
-    }
+    };
 
     const $inputs = $('.email-input');
     $inputs.on('mouseenter', handlerIn);
     $inputs.on('mouseleave', handlerOut);
 
-    const $submit = $('#submit-button button');
-    $submit.on('click', clickHandler);
-
     $("#email").emailautocomplete({
         suggClass: "autocomplete",
         domains:[""]
-      });
+    });
 
-    
     const $form = $('form');
     $form.on('submit', submitHandler);
 });
-
-
